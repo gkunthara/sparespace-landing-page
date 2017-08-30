24232370d8a671eba09f128d7e0edcffd219212a
George Kunthara <gkunthara1@gmail.com>
2017-08-30 00:41:47 -0700

    Scroll Text
    Added a timer which updates certain text on the homepage.

--- src/Header.js
+++ src/Header.js
@@ -4,24 +4,38 @@ import {Logo} from "./Logo";
 import './media.css';
 import Scroll from 'react-scroll';
 
-
+// for scrolling
 var scroll = Scroll.animateScroll;
+//list of item to show on homepage
+const items = ['mattress','loft kit', 'skis', 'boxes', 'clothes'];
+var count = 0;
 
 export class Header extends React.Component {
 
+
+    componentDidMount() {
+        const intervalId = setInterval(this.timer, 3000);
+        this.setState({intervalId: intervalId});
+    }
+    componentWillUnmount() {
+        clearInterval(this.state.intervalId)
+    }
 
 
     constructor(props) {
         super(props)
         this.state = {
-            hover: false
+            hover: false,
+            currentItem: items[count]
         }
         this.handleHover = this.handleHover.bind(this)
+        this.timer = this.timer.bind(this)
     }
 
     scrollToBottom() {
         scroll.scrollToBottom();
     }
+
 
 
 
@@ -32,8 +46,22 @@ export class Header extends React.Component {
         })
     }
 
+    //after 3 seconds, change item to showcase, then stop after
+    //all items are done showing
+    timer(){
+        count += 1
+        if(count < 4) {
+            this.setState({
+                currentItem: items[count]
+            })
+        }
+    }
+
+
+
 
     render(){
+
 
         const headerStyle = {
             fontSize: '2.5em',
@@ -74,7 +102,7 @@ export class Header extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-sm-4 text-center header" style={divStyle}>
-                        <h1 style={headerStyle}> <b> Find cheaper storage for your belongings </b> </h1>
+                        <h1 style={headerStyle}> <b> Find cheaper storage for your {this.state.currentItem} </b> </h1>
                         <button onClick={this.scrollToBottom} style={buttonStyle} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover} type="button" className="btn btn-outline">Learn More</button>
                     </div>
                     <div className="col-sm-8" style={divStyle}>
